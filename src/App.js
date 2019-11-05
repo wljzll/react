c0308b1c1fc3ad6dd812e4025223b138af09a35d
zoulele <jack.zou@happyeasygo.com>
2019-11-05 10:33:17 +0800

    react-redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import BlogList from './components/BlogList/BlogList'
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BlogList/>
+      </div>
+    )
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+
+import blog from './blog'
+
+export default combineReducers({
+  blog
+})
