02a10927389eeff2cc8328da3de6c48689101e99
wljzll <1826865187@qq.com>
2019-11-09 15:42:16 +0800

    完成blog

--- src/components/BlogList/BlogItem.js
+++ src/components/BlogList/BlogItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function BlogItem() {
+export default function BlogItem(props) {
   return (
     <li>
-      
+      <span>{props.userId}</span>
+      <h1>{props.title}</h1>
+      <h5>{props.body}</h5>
     </li>
   )
 }
--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import BlogItem from './BlogItem'
 
- class BlogList extends Component {
-  render() {
-    console.log(this.props)
-    return (
-      <ul>
-        <BlogItem/>
+import {fetchBlogList} from '../../actions/blog'
 
+ class BlogList extends Component {
+  
+  componentDidMount () {
+    this.props.fetchBlogList()
+  }
+  render() {
+    const {list,isloading}=this.props
+    return (
+      isloading ? 
+      <div>isloading</div>
+      :
+      <ul>
+      {
+       list.map(item => {
+         return  <BlogItem key={item.id} {...item}/>
+        })
+      }
       </ul>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  list:state.blog.list,
+  isloading:state.blog.isloading
+})
 
-export default connect()(BlogList)
+export default connect(mapStateToProps,{fetchBlogList})(BlogList)
